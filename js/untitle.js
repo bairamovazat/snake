9f908d31db1e7f7f18bd2869ac56a4e207d9b1a6
bairamovazat <bairamovazat@gmail.com>
2017-01-08 02:02:31 +0300

    добавлен swipe

--- js/untitle.js
+++ js/untitle.js
@@ -14,21 +14,20 @@ class Swipe{
 		this.x;
 		this.y;
 		var obj = this;
-		window.addEventListener("mousedown", function(e){obj.swipeStart(e)});
-		window.addEventListener("mouseup" , function(e){obj.swipeEnd(e)});
+		window.addEventListener("touchstart", function(e){obj.swipeStart(e)});
+		window.addEventListener("touchend" , function(e){obj.swipeEnd(e)});
 	}
 	swipeStart(e){
-		this.lastX = e.clientX;
-		this.lastY = e.clientY;
+		this.lastX = e.changedTouches[0].pageX;;
+		this.lastY = e.changedTouches[0].pageY;;
 	}
 	swipeEnd(e){
-		var currentX = e.clientX;
-		var currentY = e.clientY; 
+		var currentX = e.changedTouches[0].pageX;
+		var currentY = e.changedTouches[0].pageY; 
 		var dX = this.lastX - currentX;
 		var dY = this.lastY - currentY;
 		dX*= (dX < 0) ? (-1) : 1;
 		dY*= (dY < 0) ? (-1) : 1;
-		alert(dX + ", " + dY);
-
+		alert("dX = " + dX + ", dY = " + dY);
 	}
 }
