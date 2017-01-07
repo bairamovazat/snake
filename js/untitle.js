7a6101140ec57b2f1801a6b9e76b3eee18cfa624
bairamovazat <bairamovazat@gmail.com>
2017-01-08 02:16:22 +0300

    проверка swipe

--- js/untitle.js
+++ js/untitle.js
@@ -13,6 +13,7 @@ class Swipe{
 	constructor(){
 		this.x;
 		this.y;
+		this.swipesArray = [];
 		var obj = this;
 		window.addEventListener("touchstart", function(e){obj.swipeStart(e)});
 		window.addEventListener("touchend" , function(e){obj.swipeEnd(e)});
@@ -28,6 +29,20 @@ class Swipe{
 		var dY = this.lastY - currentY;
 		dX*= (dX < 0) ? (-1) : 1;
 		dY*= (dY < 0) ? (-1) : 1;
-		alert("dX = " + dX + ", dY = " + dY);
+		this.swipesArray.push({"x":dX,"y":dY});
+		console.log(this.swipesArray);
+		if(this.swipesArray.length % 10 == 0 ){
+			var minX = this.swipesArray[0]["x"];
+			var minY = this.swipesArray[0]["y"];
+			for(var key in this.swipesArray){
+				if(key["x"] < minX){
+					minX = this.swipeArray[key]["x"]
+				}
+				if(key["y"] < minY){
+					minY = this.swipeArray[key]["y"]
+				}
+			}
+			alert("minX = " + minX + ", minY = " + minY)
+		}
 	}
 }
